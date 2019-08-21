6d9e416ff852f460b834a7759e10609eb286c166
KeishiKITAGAWA <kaynekk0115@gmail.com>
2019-08-21 16:48:43 +0900

    fixed op/ed

--- ending/director.rb
+++ ending/director.rb
@@ -1,12 +1,14 @@
-require 'dxruby'
+module Ending
+  class Director
+    def initialize
+      font = Font.new(66)
+      font2 = Font.new(22)
+    end
 
-Window.width = 900
-Window.height = 650
-font = Font.new(66)
-font2 = Font.new(22)
-
-Window.loop do
-  Window.draw_font(300,300,"おしまい",font)
-  Window.draw_font(600,500,"spaceキーを押して終了",font2)
-  break if Input.key_push?(K_SPACE)
+    def play
+      Window.draw_font(300,300,"おしまい",font)
+      Window.draw_font(600,500,"spaceキーを押して終了",font2)
+      break if Input.key_push?(K_SPACE)
+    end
+  end
 end
