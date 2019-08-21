3fa1216d836eaccd81182b8d868ae988736d6e6d
KeishiKITAGAWA <kaynekk0115@gmail.com>
2019-08-21 16:12:05 +0900

    fix

--- So-ten-quest/maps/load_maps.rb
+++ So-ten-quest/maps/load_maps.rb
@@ -2,7 +2,7 @@
 require_relative 'north_west/director'
 require_relative 'north_east/director'
 require_relative 'south_west/director'
-require_relative 'oki/director'
+require_relative 'Oki/director'
 
 Map.add(North_west::Director.new,  :north_west)
 Map.add(North_east::Director.new,  :north_east)
--- opening/director.rb
+++ opening/director.rb
@@ -13,10 +13,14 @@ title = Font.new(64)
 small_message = Font.new(25)
 
 Window.loop do
-    Window.draw_font(200, 270, "So-ten_Quest", title)
-    Window.draw_font(400, 340, "press SPACE to continue", small_message, color:[255, 255, 0, 255])
+    Window.draw_font(250, 270, "So-ten_Quest", title)
+    Window.draw_font(345, 340, "press SPACE to continue", small_message, color:[255, 255, 0, 255])
 
     if Input.key_push?(K_SPACE) then
         Break #本来であればここでマップ(サンレイク)への遷移を行う
+    end
+
+    if Input.key_push?(K_ESCAPE) then # escキーを押すと終了する
+        Break
     end
 end
